d53525d5dfcfd29b9fc7854c61106eaa1f94be07
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-04-09 19:10:02 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
   end
   
   post '/piglatinize' do
-    @piglatinized_text = PigLatinizer.new(params[:user_phrase])
+    @piglatinized_text = PigLatinizer.new
     erb :user_input
   end
 end
