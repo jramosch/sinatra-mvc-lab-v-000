3b65094d5b885ee163e6ea9319dc05a0c1a6a8fc
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-04-09 16:41:19 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,7 +3,7 @@ class PigLatinizer
   def piglatinize(word)
     vowels = ["a","e","i","o","u"]
     if word.downcase.match(/\A[aeiou]/) == nil
-      letters = word.slice!(/\A[^aeiou]./)
+      letters = word.slice!(/\A[^aeiou]*/)
       letters << "ay"
       word << letters
     else
